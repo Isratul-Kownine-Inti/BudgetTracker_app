3ed8d40797bfa16d2999b4f9ae7419824252dd9a
Isratul-Kownine-Inti <isratulinti24@gmail.com>
2024-08-27 23:47:48 +0600

    completed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,45 @@
+import { Button } from "flowbite-react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from '../context/Auth';
+
 export default function Navbar() {
+  const navigate = useNavigate();
+  
+
+  const gotoSignOut = async () => {
+    const yesDelete = window.confirm("Are you sure you want to sign out?");
+    if (yesDelete) {
+      try {
+        const res = await fetch('http://localhost:3000/api/user/auth/logout', { 
+          method: "POST",
+          headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (!res.ok) {
+          const error = await res.json();
+          console.error("Error:", error);
+        } else {
+        
+          localStorage.clear();
+          navigate('/');
+        }
+      } catch (error) {
+        console.error("Network error:", error);
+      }
+    }
+  };
+
+
   return (
     <div className="bg-violet-700/30 py-2">
       <div className="mx-auto flex max-w-7xl items-center justify-between px-5 text-white">
         <h2 className="flex items-center italic">
-          <img src="/logo.svg" height="40" width="40" />
+          <img src="/logo.svg" height="40" width="40" alt="Logo" />
           <span>Budget App</span>
         </h2>
         <div className="flex items-center gap-4">
           <h2>Isratul Kownine Inti</h2>
-          <a href="#" className="undeline text-sm text-white/50">
-            Sign Out
-          </a>
+          <Button onClick={gotoSignOut}>Sign Out</Button>
         </div>
       </div>
     </div>
